228339386b8caf12f159143297e0614125768f1f
Marcos Antunes <antunesmg@hotmail.com>
2015-12-20 19:37:02 -0200

    mapa adicionado na main

--- AppRedes/www/js/MainCtrl.js
+++ AppRedes/www/js/MainCtrl.js
@@ -1,9 +1,23 @@
 angular.module('AppRedes')
 
 
-.controller('MainCtrl', function ($scope){ 
-  console.log('teste');
-  $scope.Login = function () {
+.controller('MainCtrl', function ($scope){
+
+        var myLatlng = new google.maps.LatLng(37.3000, -120.4833);
+ 
+        var mapOptions = {
+            center: myLatlng,
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+ 
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+        $scope.map = map;
+   
+
+  	console.log('teste');
+  	$scope.Login = function () {
     console.log('login');
     }; 
 
